c338178f63acc691a393d9576f2ada5cf0d5817c
Basaingeal <michaeljcurrie136@gmail.com>
2019-01-09 20:30:34 -0600

    Add Clean project structure projects

---
+++ src/Nursry.Core/Nursry.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Nursry.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Nursry.Infrastructure/Nursry.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Nursry.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
