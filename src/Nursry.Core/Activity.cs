d28993d7c3675c2c4917d8fe3cfc7e37712283e4
Michael J. Currie <michaeljcurrie136@gmail.com>
2019-01-11 11:34:38 -0600

    remove redundant folders from structure (#2)

---
+++ src/Nursry.Core/Activity.cs
@@ -0,0 +1,14 @@
+﻿using Nursry.Core.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nursry.Core
+{
+    public class Activity : BaseEntity
+    {
+        public string Name { get; set; }
+        public DateTime TimeStarted { get; set; }
+        public DateTime TimeEnded { get; set; }
+    }
+}
---
+++ src/Nursry.Core/SharedKernel/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nursry.Core.SharedKernel
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
