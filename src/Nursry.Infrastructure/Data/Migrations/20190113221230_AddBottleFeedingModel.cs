af239e0978a62772a34402c3c4243c6e8b91c391
Michael j. Currie <mcurrie@agwsinc.com>
2019-01-15 10:45:55 -0600

    sync with master migrations

---
+++ src/Nursry.Infrastructure/Data/Migrations/20190113221230_AddBottleFeedingModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Nursry.Infrastructure.Data.Migrations
+{
+    public partial class AddBottleFeedingModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Amount",
+                table: "Log",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ContentsId",
+                table: "Log",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Log_ContentsId",
+                table: "Log",
+                column: "ContentsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Log_BottleContents_ContentsId",
+                table: "Log",
+                column: "ContentsId",
+                principalTable: "BottleContents",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Log_BottleContents_ContentsId",
+                table: "Log");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Log_ContentsId",
+                table: "Log");
+
+            migrationBuilder.DropColumn(
+                name: "Amount",
+                table: "Log");
+
+            migrationBuilder.DropColumn(
+                name: "ContentsId",
+                table: "Log");
+        }
+    }
+}
